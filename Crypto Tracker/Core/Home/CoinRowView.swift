3812fb9c63f884dc33331c1aa513e17773861dff
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-11-28 01:24:33 +0600

    Double Extension

--- Crypto Tracker/Core/Home/CoinRowView.swift
+++ Crypto Tracker/Core/Home/CoinRowView.swift
@@ -9,27 +9,54 @@ import SwiftUI
 
 struct CoinRowView: View {
     let coin: CoinModel
+    let showCenterColumn: Bool
     var body: some View {
         HStack(spacing: 12) {
+            leftColumn()
+            Spacer()
+            if showCenterColumn {
+                centerColumn()
+            }
+            Spacer()
+            rightColumn()
+        }
+        .padding()
+        .font(.subheadline)
+    }
+}
+
+#Preview {
+    CoinRowView(coin: .placeHolder, showCenterColumn: true)
+}
+
+extension CoinRowView {
+    private func leftColumn() -> some View {
+        HStack {
             Text("\(coin.rank)")
             Circle()
                 .frame(width: 30, height: 30)
             Text(coin.symbol.uppercased())
                 .font(.headline)
-            Spacer()
-            VStack(alignment: .trailing) {
-                Text("\(coin.currentPrice)")
-                    .font(.headline)
-                Text("\(coin.priceChangePercentage24H ?? 0)%")
-                    .foregroundStyle(
-                        (coin.priceChangePercentage24H ?? 0) >= 0 ? .green : .red
-                    )
-            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    CoinRowView(coin: .placeHolder)
+    
+    private func centerColumn() -> some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .font(.headline)
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+    }
+    
+    private func rightColumn() -> some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith2Decimals())
+                .font(.headline)
+            Text((coin.priceChangePercentage24H ?? 0).asPercentString())
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ? .green : .red
+                )
+        }
+        
+    }
 }
---
+++ Crypto Tracker/Extensions/Double.swift
@@ -0,0 +1,40 @@
+//
+//  Double.swift
+//  Crypto Tracker
+//
+//  Created by shopnil hasan on 28/11/24.
+//
+
+
+import Foundation
+
+extension Double {
+    
+    /// Converts a Double into a Currency with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "usd"
+        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
+}
