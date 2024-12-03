41b25aadbe2df328c2039433738712d60d665181
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-12-03 23:43:30 +0600

    Ipad Ui Views issue

---
+++ Crypto Tracker/Core/Home/Views/HomeStatView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeStatView.swift
+//  Crypto Tracker
+//
+//  Created by shopnil hasan on 3/12/24.
+//
+
+import SwiftUI
+
+struct HomeStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeStatView()
+}
